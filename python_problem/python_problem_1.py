23324a47671ca0f276ed0a6a31d22578b08e9524
krlyeon <sooyeon8427@gmail.com>
2025-01-03 05:26:38 +0900

    31게임-8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,11 +1,9 @@
-num = 0
-br_num = 0
 
-while br_num < 31:
+def brGame(player, br_num):
     while 1:
-        A_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-        if A_input.isdigit():
-            num = int(A_input)
+        player_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        if player_input.isdigit():
+            num = int(player_input)
             if num in [1, 2, 3]:
                 break
             else:
@@ -15,25 +13,19 @@ while br_num < 31:
 
     for i in range(num):
         br_num += 1
-        print("playerA :",br_num)
+        print(player + " : " + str(br_num))
         if br_num == 31:
-            print("playerB win!")
-            exit()
-
-    while 1:
-        B_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-        if B_input.isdigit():
-            num = int(B_input)
-            if num in [1, 2, 3]:
-                break
+            if player == "playerA":
+                print("playerB win!")
             else:
-                print("1, 2, 3 중 하나를 입력하세요.")
-        else:
-            print("정수를 입력하세요")
-
-    for j in range(num):
-        br_num += 1
-        print("playerB :",br_num)
-        if br_num == 31:
-            print("playerA win!")
+                print("playerA win!")
             exit()
+    return br_num
+
+    
+
+num = 0
+br_num = 0
+while br_num < 31:
+    br_num = brGame("playerA", br_num)
+    br_num = brGame("playerB", br_num)
