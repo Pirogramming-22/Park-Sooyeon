645d7e217b7730e4563d71d1737a4c0bbfe94bea
krlyeon <sooyeon8427@gmail.com>
2025-01-03 05:04:05 +0900

    31게임-5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,5 @@
 num = 0
+br_num = 0
 
 while 1:
     A_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
@@ -12,5 +13,20 @@ while 1:
         print("정수를 입력하세요")
 
 for i in range(num):
-    i += 1
-    print("playerA :",i)
+    br_num += 1
+    print("playerA :",br_num)
+
+while 1:
+    B_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+    if B_input.isdigit():
+        num = int(B_input)
+        if num in [1, 2, 3]:
+            break
+        else:
+            print("1, 2, 3 중 하나를 입력하세요.")
+    else:
+        print("정수를 입력하세요")
+
+for j in range(num):
+    br_num += 1
+    print("playerB :",br_num)
